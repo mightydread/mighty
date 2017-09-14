1083cc4013f562db6df05fed09781386d1c479bd
mightydread <mightydreadmario@gmail.com>
2017-09-14 22:18:12 +0200

    playing with anim

--- js/main.js
+++ js/main.js
@@ -1,23 +1,48 @@
+var svg_anim = new Array('');
 
-$(document).ready(function(){
-    $(".m").click(function(){
-        window.open("http://soundcloud.com/mightydread",'_blank');
+var vivus_init = function(objects) {
+  for (var i = 0; i < objects.length; i++) {
+    svg_anim[i] = new Vivus(objects[i], {
+      duration: 100,
+      start: 'manual',
+      reverseStack: true
+    }, function(obj) {
+      obj.el.classList.add('finished');
+      obj.el.style.fillOpacity = 1;
+      obj.el.style.strokeOpacity = 0;
     });
-    $(".m").mouseenter(function(){
-        $("#mighty").hide();
-        $("#music").show();
-    });
-    $(".m").mouseleave(function(){
-        $("#music").hide();
-        $("#mighty").show();
-    });
-    $(".i").mouseenter(function(){
-        $("#mighty").hide();
-        $("#it").show();
+  }
+};
+function draw_all() {
+    console.log(svg_anim);
+    for(var i=0;i<svg_anim.length;i++) {
+      svg_anim[i].play();
+    }
+  }
+$(document).ready(function() {
+    vivus_init($(".anim"));
+    svg_anim[0].play();
+  $(".m").click(function() {
+    window.open("http://soundcloud.com/mightydread", '_blank');
+  });
+  $(".m").mouseenter(function() {
+    svg_anim[0].play(-1);
+    $("#mighty").hide();
+    $("#music").show();
+    svg_anim[1].play();
+  });
+  $(".m").mouseleave(function() {
+    $("#music").hide();
+    $("#mighty").show();
+  });
+  $(".i").mouseenter(function() {
+    $("#mighty").hide();
+    $("#it").show();
+    svg_anim[2].play();
 
-    });
-    $(".i").mouseleave(function(){
-        $("#it").hide();
-        $("#mighty").show();
-    });
+  });
+  $(".i").mouseleave(function() {
+    $("#it").hide();
+    $("#mighty").show();
+  });
 });
